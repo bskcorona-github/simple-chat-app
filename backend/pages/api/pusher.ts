358c3ca631784ea425ffaba99addb51e5df0f12d
kinjotatsuro <kinjo.tatsuro@rue.co.jp>
2024-11-26 16:23:12 +0900

    aiu

--- backend/pages/api/pusher.ts
+++ backend/pages/api/pusher.ts
@@ -6,24 +6,20 @@ const pusher = new Pusher({
   key: process.env.PUSHER_KEY!,
   secret: process.env.PUSHER_SECRET!,
   cluster: process.env.PUSHER_CLUSTER!,
-  useTLS: true, // TLSを有効化
+  useTLS: true,
 });
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    if (req.method === "POST") {
-      try {
-        console.log("Request body:", req.body);
-        const { channel, event, data } = req.body;
-  
-        await pusher.trigger(channel, event, data);
-  
-        res.status(200).json({ message: "Event triggered" });
-      } catch (error) {
-        console.error("Error triggering Pusher event:", error);
-        res.status(500).json({ message: "Failed to trigger event", error });
-      }
-    } else {
-      res.status(405).json({ message: "Method not allowed" });
+  if (req.method === "POST") {
+    try {
+      const { channel, event, data } = req.body;
+      await pusher.trigger(channel, event, data);
+      res.status(200).json({ message: "Event triggered" });
+    } catch (error) {
+      console.error("Error triggering Pusher event:", error);
+      res.status(500).json({ message: "Failed to trigger event", error });
     }
+  } else {
+    res.status(405).json({ message: "Method not allowed" });
   }
-  
+}
